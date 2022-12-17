a5f7437df553867292121089de900bcb482e47c1
kyawoo-23 <kyawkyawjek@gmail.com>
2022-12-17 23:37:05 +0630

    some ui changes

--- src/components/MenuBar/index.jsx
+++ src/components/MenuBar/index.jsx
@@ -11,7 +11,7 @@ const MenuBar = ({ id, setId }) => {
     }
 
     return (
-        <div className="w-full bg-black py-8 flex flex-wrap gap-8 items-center justify-between">
+        <div className="w-full py-8 flex flex-wrap gap-8 items-center justify-between">
             <h1 className="text-white text-2xl">Pokemon Card Generator</h1>
             <button
                 className="flex items-center gap-3 text-white text-lg border border-white px-4 py-2 rounded-md ml-auto"
